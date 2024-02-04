7ea30328ae8ea80e553e0b68a1b614ec42a02289
Tahir Ahmad <tahirahmad0234@gmail.com>
2024-02-04 16:05:49 +0600

    remove project

--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: "users/sessions",
-    registrations: "users/registrations"
-  }
-
-  get "up" => "rails/health#show", as: :rails_health_check
-end
