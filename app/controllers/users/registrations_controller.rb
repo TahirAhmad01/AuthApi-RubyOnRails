b63320478d67b478dd0d4d449171e1107c7cfdf5
TahirAhmad <tahirahmad0234@gmail.com>
2024-04-29 10:59:25 +0600

    add validation when user logged in. user can't create new user

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,30 +1,46 @@
-# frozen_string_literal: true
-
 class Users::RegistrationsController < Devise::RegistrationsController
   respond_to :json
 
-  private
-
-  def respond_with (resource, options = {})
-    # if current_user
-    #   render json: { error: "User Already Signed In. Please log out before proceeding with registration.",
-    #                  data: current_user,
-    #                  status: 200 }, status: :ok
-    # else
-      if resource.persisted?
-        render json: {
-          code: 200, message: "User registration was successfully registered", data: resource
-        }, status: :ok
-      else
-        errors_array = resource.errors.messages.map do |attribute, messages|
-          { name: attribute, errors: messages.map { |error| "#{attribute} #{error}" } }
+  def create
+    if current_user
+      render json: {
+        error: "You are already logged in",
+        data: current_user,
+        status: 401
+      }
+    else
+      super do |resource|
+        if resource.persisted?
+          return render_registration_success(resource)
+        else
+          return render_registration_failure(resource)
         end
-
-        render json: {
-          message: "User registration failed", errors: errors_array
-        }, status: :unprocessable_entity
       end
     end
+  end
 
-  # end
+  private
+
+  def render_registration_success(resource)
+    render json: {
+      code: 200,
+      message: "User registration was successful",
+      data: resource
+    }, status: :ok
+  end
+
+  def render_registration_failure(resource)
+    errors_array = resource.errors.messages.map do |attribute, messages|
+      { name: attribute, errors: messages.map { |error| "#{attribute} #{error}" } }
+    end
+
+    render json: {
+      message: "User registration failed",
+      errors: errors_array
+    }, status: :unprocessable_entity
+  end
+
+  def after_sign_up_path_for(resource)
+    user_path(resource)
+  end
 end
