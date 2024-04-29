92f2082fb0ef54230e6fd777cc57a308bd617c15
TahirAhmad <tahirahmad0234@gmail.com>
2024-04-30 00:34:12 +0600

    sign up default role added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,16 @@
 class User < ApplicationRecord
-  has_many :companies
   include Devise::JWT::RevocationStrategies::JTIMatcher
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
 
-  ROLES = %w{super_admin admin user}
+  before_validation :set_user_role, on: :create
+  has_many :companies
+
+  ROLES = %w(super_admin admin user)
+
   validates :role, inclusion: { in: ROLES }
-  # validate :password_confirmation_match, on: :create
 
   def jwt_payload
     super
@@ -23,7 +24,7 @@ class User < ApplicationRecord
 
   private
 
-  # def password_confirmation_match
-  #   errors.add(:password_confirmation, "doesn't match Password") if password != password_confirmation
-  # end
+  def set_user_role
+    self.role ||= 'user'
+  end
 end
